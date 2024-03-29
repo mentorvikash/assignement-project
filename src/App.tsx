f42c65c36143a544f7e872183d23b64199680e08
mentorvikash <mentorvikash@gmail.com>
2024-03-29 13:43:07 +0530

    implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import "./App.css";
-import FormData from "./Components/Form";
-import ProductList from "./Components/Product";
 import Nav from "./Components/Nav";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+
 function App() {
   return (
     <>
       <Nav />
-      <h1 className="text-3xl font-bold underline"></h1>
-      <div className="flex flex-col gap-5 align-middle m-4">
-        <div className="mb-5 w-full flex justify-center">
-          <img className="" src="src/assets/gift.png" alt="box" width={100} />
-        </div>
-        <div className="mb-1 text-center w-full">
-          <h2 className="font-semibold text-4xl ">
-            Tell us about your gift recipient
-          </h2>
-        </div>
-        <FormData />
-        <ProductList />
-      </div>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
     </>
   );
 }
